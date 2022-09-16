cf13541a5887c40c5697cd014b1529cd449b74f0
Kenny Depecker <kenny@libaro.be>
2022-09-16 14:16:34 +0200

    created HasThing Trait

---
+++ src/Traits/HasThing.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Libaro\IoTManager\Traits;
+
+trait HasThing
+{
+
+    /**
+     * Will activate Thing in AWS
+     *
+     * @return void
+     */
+    public function activate()
+    {
+
+    }
+
+    /**
+     * Will deactivate Thing in AWS
+     *
+     * @return void
+     */
+    public function deactivate()
+    {
+
+    }
+
+    /**
+     * Generate necessary certificates for Thing in AWS
+     *
+     * @return void
+     */
+    public function generateCertificates()
+    {
+
+    }
+}
